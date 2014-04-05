b50c5a189a59786263c3c3e96302968e26afead4
Harinandan <hari.katam@gmail.com>
2014-04-06 01:19:35 +0530

    Rod restriction and code min

--- Project/src/dominos.cpp
+++ Project/src/dominos.cpp
@@ -139,10 +139,10 @@ void dominos_t::resetUp()
 				//checkUp(m_joint->GetAnchorB(), m_joint->GetAnchorA(), m_joint);
 		switch(key){
 		case 'y':
-		rod[3]->ApplyLinearImpulse(b2Vec2(0,10),rod[3]->GetPosition(),100);
+		rod[3]->ApplyLinearImpulse(b2Vec2(0,40),rod[3]->GetPosition(),100);
 		break;
 		case 'h':
-		rod[3]->ApplyLinearImpulse(b2Vec2(0,-10),rod[3]->GetPosition(),100);
+		rod[3]->ApplyLinearImpulse(b2Vec2(0,-40),rod[3]->GetPosition(),100);
 		break;
 		case ' ':
 		rod[3]->SetLinearVelocity(b2Vec2(0,0));
@@ -337,136 +337,53 @@ void dominos_t::resetUp()
 	b2BodyDef bd_player1[11];
 	{
 		
-		bd_player1[0].position.Set(-25.0f,20.0f);
-		team1[0] = m_world->CreateBody(&bd_player1[0]);
-		player1[0].SetAsBox(1,1.5,b2Vec2(0.0f,0.0f),0.0f);
-		team1[0]->CreateFixture(&player1[0],0.0f);
-		
-		bd_player1[1].position.Set(-18.0f,20.0f);
-		team1[1] = m_world->CreateBody(&bd_player1[1]);
-		player1[1].SetAsBox(1,1.5,b2Vec2(0.0f,5.0f),0.0f);
-		team1[1]->CreateFixture(&player1[1],0.0f);
-		
-		bd_player1[2].position.Set(-18.0f,20.0f);
-		team1[2] = m_world->CreateBody(&bd_player1[2]);
-		player1[2].SetAsBox(1,1.5,b2Vec2(0.0f,-5.0f),0.0f);
-		team1[2]->CreateFixture(&player1[2],0.0f);
-		
-		bd_player1[3].position.Set(-4.0f,20.0f);
-		team1[3] = m_world->CreateBody(&bd_player1[3]);
-		player1[3].SetAsBox(1,1.5,b2Vec2(0.0f,12.0f),0.0f);
-		team1[3]->CreateFixture(&player1[3],0.0f);
-		
-		bd_player1[4].position.Set(-4.0f,20.0f);
-		team1[4] = m_world->CreateBody(&bd_player1[4]);
-		player1[4].SetAsBox(1,1.5,b2Vec2(0.0f,6.0f),0.0f);
-		team1[4]->CreateFixture(&player1[4],0.0f);
-		
-		bd_player1[5].position.Set(-4.0f,20.0f);
-		team1[5] = m_world->CreateBody(&bd_player1[5]);
-		player1[5].SetAsBox(1,1.5,b2Vec2(0.0f,0.0f),0.0f);
-		team1[5]->CreateFixture(&player1[5],0.0f);
-		
-		bd_player1[6].position.Set(-4.0f,20.0f);
-		team1[6] = m_world->CreateBody(&bd_player1[6]);
-		player1[6].SetAsBox(1,1.5,b2Vec2(0.0f,-6.0f),0.0f);
-		team1[6]->CreateFixture(&player1[6],0.0f);
-		
-		bd_player1[7].position.Set(-4.0f,20.0f);
-		team1[7] = m_world->CreateBody(&bd_player1[7]);
-		player1[7].SetAsBox(1,1.5,b2Vec2(0.0f,-12.0f),0.0f);
-		team1[7]->CreateFixture(&player1[7],0.0f);
-		
-		bd_player1[8].position.Set(11.0f,20.0f);
-		team1[8] = m_world->CreateBody(&bd_player1[8]);
-		player1[8].SetAsBox(1,1.5,b2Vec2(0.0f,8.0f),0.0f);
-		team1[8]->CreateFixture(&player1[8],0.0f);
-		
-		bd_player1[9].position.Set(11.0f,20.0f);
-		team1[9] = m_world->CreateBody(&bd_player1[9]);
-		player1[9].SetAsBox(1,1.5,b2Vec2(0.0f,0.0f),0.0f);
-		team1[9]->CreateFixture(&player1[9],0.0f);
-		
-		bd_player1[10].position.Set(11.0f,20.0f);
-		team1[10] = m_world->CreateBody(&bd_player1[10]);
-		player1[10].SetAsBox(1,1.5,b2Vec2(0.0f,-8.0f),0.0f);
-		team1[10]->CreateFixture(&player1[10],0.0f);
+		int x[11],y[11];
+		x[0]=-25;y[0]=0;
+		x[1]=-18;y[1]=5;
+		x[2]=-18;y[2]=-5;
+		x[3]=-4;y[3]=12;
+		x[4]=-4;y[4]=6;
+		x[5]=-4;y[5]=0;
+		x[6]=-4;y[6]=-6;
+		x[7]=-4;y[7]=-12;
+		x[8]=11;y[8]=8;
+		x[9]=11;y[9]=0;
+		x[10]=11;y[10]=-8;
+		for(int i=0;i<11;i++){
+			bd_player1[i].position.Set(x[i],20.0f);
+			team1[i] = m_world->CreateBody(&bd_player1[i]);
+			player1[i].SetAsBox(0.8,1.2,b2Vec2(0.0f,y[i]),0.0f);
+			team1[i]->CreateFixture(&player1[i],0.0f);
+		}
 	}
 	
 /////////////////////////////////////////////////////////////////////////////
 	
 	//Players Team2
-b2Body* team2[11], *team2join[11]; 
+	b2Body* team2[11], *team2join[11]; 
 	b2PolygonShape player2[11];
 	b2BodyDef bd_player2[11];
 	{
-		
-		bd_player2[0].position.Set(25.0f,20.0f);
-		team2join[0] = m_world->CreateBody(&bd_player2[0]);
-		bd_player2[0].type=b2_dynamicBody;
-		team2[0] = m_world->CreateBody(&bd_player2[0]);
-		player2[0].SetAsBox(1,1.5,b2Vec2(0.0f,0.0f),0.0f);
-		team2[0]->CreateFixture(&player2[0],0.0f);
-		
-		bd_player2[1].position.Set(18.0f,20.0f);
-		team2[1] = m_world->CreateBody(&bd_player2[1]);
-		team2join[1] = m_world->CreateBody(&bd_player2[1]);
-		player2[1].SetAsBox(1,1.5,b2Vec2(0.0f,5.0f),0.0f);
-		team2[1]->CreateFixture(&player2[1],0.0f);
-		
-		bd_player2[2].position.Set(18.0f,20.0f);
-		team2[2] = m_world->CreateBody(&bd_player2[2]);
-		team2join[2] = m_world->CreateBody(&bd_player2[2]);
-		player2[2].SetAsBox(1,1.5,b2Vec2(0.0f,-5.0f),0.0f);
-		team2[2]->CreateFixture(&player2[2],0.0f);
-		
-		bd_player2[3].position.Set(4.0f,20.0f);
-		team2[3] = m_world->CreateBody(&bd_player2[3]);
-		team2join[3] = m_world->CreateBody(&bd_player2[3]);
-		player2[3].SetAsBox(1,1.5,b2Vec2(0.0f,12.0f),0.0f);
-		team2[3]->CreateFixture(&player2[3],0.0f);
-		
-		bd_player2[4].position.Set(4.0f,20.0f);
-		team2[4] = m_world->CreateBody(&bd_player2[4]);
-		team2join[4] = m_world->CreateBody(&bd_player2[4]);
-		player2[4].SetAsBox(1,1.5,b2Vec2(0.0f,6.0f),0.0f);
-		team2[4]->CreateFixture(&player2[4],0.0f);
-		
-		bd_player2[5].position.Set(4.0f,20.0f);
-		team2[5] = m_world->CreateBody(&bd_player2[5]);
-		team2join[5] = m_world->CreateBody(&bd_player2[5]);
-		player2[5].SetAsBox(1,1.5,b2Vec2(0.0f,0.0f),0.0f);
-		team2[5]->CreateFixture(&player2[5],0.0f);
-		
-		bd_player2[6].position.Set(4.0f,20.0f);
-		team2[6] = m_world->CreateBody(&bd_player2[6]);
-		team2join[6] = m_world->CreateBody(&bd_player2[6]);
-		player2[6].SetAsBox(1,1.5,b2Vec2(0.0f,-6.0f),0.0f);
-		team2[6]->CreateFixture(&player2[6],0.0f);
-		
-		bd_player2[7].position.Set(4.0f,20.0f);
-		team2[7] = m_world->CreateBody(&bd_player2[7]);
-		team2join[7] = m_world->CreateBody(&bd_player2[7]);
-		player2[7].SetAsBox(1,1.5,b2Vec2(0.0f,-12.0f),0.0f);
-		team2[7]->CreateFixture(&player2[7],0.0f);
-		
-		bd_player2[8].position.Set(-11.0f,20.0f);
-		team2[8] = m_world->CreateBody(&bd_player2[8]);
-		team2join[8] = m_world->CreateBody(&bd_player2[8]);
-		player2[8].SetAsBox(1,1.5,b2Vec2(0.0f,8.0f),0.0f);
-		team2[8]->CreateFixture(&player2[8],0.0f);
-		
-		bd_player2[9].position.Set(-11.0f,20.0f);
-		team2[9] = m_world->CreateBody(&bd_player2[9]);
-		team2join[9] = m_world->CreateBody(&bd_player2[9]);
-		player2[9].SetAsBox(1,1.5,b2Vec2(0.0f,0.0f),0.0f);
-		team2[9]->CreateFixture(&player2[9],0.0f);
-		
-		bd_player2[10].position.Set(-11.0f,20.0f);
-		team2[10] = m_world->CreateBody(&bd_player2[10]);
-		team2join[10] = m_world->CreateBody(&bd_player2[10]);
-		player2[10].SetAsBox(1,1.5,b2Vec2(0.0f,-8.0f),0.0f);
-		team2[10]->CreateFixture(&player2[10],0.0f);
+		int x[11],y[11];
+		x[0]=25;y[0]=0;
+		x[1]=18;y[1]=5;
+		x[2]=18;y[2]=-5;
+		x[3]=4;y[3]=12;
+		x[4]=4;y[4]=6;
+		x[5]=4;y[5]=0;
+		x[6]=4;y[6]=-6;
+		x[7]=4;y[7]=-12;
+		x[8]=-11;y[8]=8;
+		x[9]=-11;y[9]=0;
+		x[10]=-11;y[10]=-8;
+		for(int i=0;i<11;i++){
+			bd_player2[i].position.Set(x[i],20.0f);
+			team2join[i] = m_world->CreateBody(&bd_player2[i]);
+			bd_player2[i].type=b2_dynamicBody;
+			team2[i] = m_world->CreateBody(&bd_player2[i]);
+			player2[i].SetAsBox(0.8,1.2,b2Vec2(0.0f,y[i]),0.0f);
+			team2[i]->CreateFixture(&player2[i],0.0f);
+		}
 
 			 
 		// Team 2 Rods
@@ -482,7 +399,7 @@ b2Body* team2[11], *team2join[11];
 				pos=-11.0f;
 			}
 			else if(i==1){
-			pos=4.0f;
+				pos=4.0f;
 			}
 			else if(i==2){
 				pos=18.0f;
@@ -491,10 +408,10 @@ b2Body* team2[11], *team2join[11];
 			pos= 25.0f;
 			}
 			bd_rod_1.position.Set(pos,20.0f);
-			bd_constr[0].position.Set(pos+0.7f,6.0f);
-			bd_constr[1].position.Set(pos-0.7f,6.0f);
-			bd_constr[2].position.Set(pos+0.7f,34.0f);
-			bd_constr[3].position.Set(pos-0.7f,34.0f);
+			bd_constr[0].position.Set(pos+0.8f,6.0f);
+			bd_constr[1].position.Set(pos-0.8f,6.0f);
+			bd_constr[2].position.Set(pos+0.8f,34.0f);
+			bd_constr[3].position.Set(pos-0.8f,34.0f);
 			
 			b2FixtureDef *fd = new b2FixtureDef;
 			fd->shape = &constr;
@@ -502,20 +419,23 @@ b2Body* team2[11], *team2join[11];
 			fd->filter.categoryBits = 0x0002;
 			fd->filter.maskBits = 0x0002;
 			b2Body* con;
-			for(int j=0;j<4;j++)
-			{
+			for(int j=0;j<4;j++){
 				constr.SetAsBox(0.4f,0.4f,b2Vec2(0.0f,0.0f),0.0f);
 				bd_constr[j].type=b2_staticBody;
 				con=m_world->CreateBody(&bd_constr[j]);
 				con->CreateFixture(fd);
 			}
+			
+			
 			fd->shape = &rod1;
 			rod1.SetAsBox(0.4f,18.0f,b2Vec2(0.0f,0.0f),0.0f);
 			rod[i] = m_world->CreateBody(&bd_rod_1);
 			rod[i]->CreateFixture(fd);
 			rod1.SetAsBox(0.8f,3.0f,b2Vec2(0.0f,21.0f),0.0f);
 			rod[i]->CreateFixture(&rod1,0.0f);
-			}
+			rod1.SetAsBox(0.6f,0.2f,b2Vec2(0.0f,-18.3f),0.0f);
+			rod[i]->CreateFixture(&rod1,0.0f);
+		}
 			b2PrismaticJointDef prismaticJointDef;
 			  prismaticJointDef.bodyA = team2[0];
 			  prismaticJointDef.bodyB = rod[3];
@@ -577,26 +497,6 @@ b2Body* team2[11], *team2join[11];
 			rod->CreateFixture(&rod1,0.0f);
 		}
 	}
-	
-/////////////////////////////////////////////////////////////////////////////
-	
-
-	/*
-	{
-		b2Body* im;
-		b2PolygonShape sh_im;
-		b2BodyDef bd_im;
-		b2FixtureDef *fd = new b2FixtureDef;
-		fd->restitution = 0;
-		im=m_world->CreateBody(&bd_im);
-		sh_im.SetAsBox(1,0.1,b2Vec2(-35,35),0.0f);
-		fd->shape = &sh_im;
-		im->CreateFixture(fd);
-		sh_im.SetAsBox(1,0.1,b2Vec2(35,5),0.0f);
-		fd->shape = &sh_im;
-		im->CreateFixture(fd);
-	
-	}*/
 	
 /////////////////////////////////////////////////////////////////////////////
 	
